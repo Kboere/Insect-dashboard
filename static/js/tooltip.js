952a1d841d8ac6714daef7751474cc9129799294
Kevin Boere <kevin.boere@hva.nl>
2024-01-24 12:41:58 +0100

    added new loading + Tooltip change

--- static/js/tooltip.js
+++ static/js/tooltip.js
@@ -1,21 +1,20 @@
 // Assuming you have removed the div with id "myDiv"
 var deleteCameraInfo = document.querySelector(".gemeente-stats.camera");
+var deleteGrafieken = document.querySelectorAll(".gemeente-stats.grafiek");
 
-// // Function to remove the div
-// function removeDiv() {
-//     if (deleteCameraInfo) {
-//         deleteCameraInfo.remove();
-//     }
-// }
-
-// Function to show the div
-function showDiv() {
-    // Check if the div already exists
+// Function to show/hide the div based on groenPercentage
+function showDiv(groenPercentage) {
+    // Check if the div with camera info already exists
     if (deleteCameraInfo) {
+        // Display "grafiek" if groenPercentage is not 0, otherwise hide it
+        deleteGrafieken.forEach(grafiek => {
+            grafiek.style.display = groenPercentage === 0 ? "none" : "flex";
+        });
+
+        // Display "camera" div
         deleteCameraInfo.style.display = "flex";
     }
 }
-
 
 function pop_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0(feature, layer) {
     layer.on({
@@ -24,21 +23,21 @@ function pop_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0(fea
                 layer.closePopup();
             } else {
                 layer.eachLayer(function (feature) {
-                    feature.closePopup()
+                    feature.closePopup();
                 });
             }
         },
         mouseover: highlightFeature,
         click: function (e) {
-            // Show the div when the layer is clicked
-            showDiv();
+            // Show the div with appropriate display settings
+            showDiv(feature.properties['complete_dataset_groenPercentage/2018']);
         }
     });
 
     let groenPercentage = feature.properties['complete_dataset_groenPercentage/2018'];
 
-    // Check if groenPercentage is 0
-    if (groenPercentage === 0) {
+       // Check if groenPercentage is 0
+       if (groenPercentage === 0) {
         // Set different content if groenPercentage is 0
         var popupContent =
             '<table>\
